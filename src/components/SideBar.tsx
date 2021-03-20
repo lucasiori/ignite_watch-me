ff1baf3185ddf3c16e72069c1e74d96c94610e05
Lucas Fernando Iori <lucasferiori@gmail.com>
2021-03-20 16:07:58 -0300

    feat: challenge completion

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,37 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+interface Genre {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  genres: Genre[]
+  selectedGenre: Genre;
+  onSelectGenre: (genreId: number) => void
+}
+
+export function SideBar({ 
+  genres,
+  selectedGenre,
+  onSelectGenre
+}: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onSelectGenre(genre.id)}
+            selected={selectedGenre.id === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
